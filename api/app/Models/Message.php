130e7a4dc9ef32f65cee2b828615fc79b53b4664
Diogo Souza <diogo.souza@msn.com>
2021-04-05 23:18:02 -0400

    refactor: remoção do atributo 'date' do guarded

--- api/app/Models/Message.php
+++ api/app/Models/Message.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     protected $table = 'messages';
-    protected $guarded = ['id', 'date'];
+    protected $guarded = ['id'];
     public $timestamps = false;
 }
