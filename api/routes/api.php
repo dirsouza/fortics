bfa7b6e29fe0a9af4abb8c556c8958d6bf41c758
Diogo Souza <diogo.souza@msn.com>
2021-04-07 19:30:38 -0400

    refactor: revisão na rota POST de criacao

--- api/routes/api.php
+++ api/routes/api.php
@@ -6,7 +6,7 @@ Route::prefix('v1')->group(function () {
 
     Route::prefix('sessions')->group(function () {
         Route::get('/', 'SessionController@list')->name('session.list');
-        Route::post('create', 'SessionController@create')->name('session.create');
+        Route::post('/', 'SessionController@create')->name('session.create');
         Route::delete('{identifier}', 'SessionController@delete')->name('session.delete');
         Route::post('upload', 'SessionController@upload')->name('session.upload');
     });
